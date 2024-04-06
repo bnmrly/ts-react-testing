0fd11dbe0d9d0cad6ab128431b90e8755979864e
bnmrly <benjaminjohnmarley@gmail.com>
2024-04-06 14:36:41 +0100

    test Person

--- src/Person.test.tsx
+++ src/Person.test.tsx
@@ -3,8 +3,8 @@ import { render, screen } from "@testing-library/react";
 import { Person } from "./Person";
 
 test("renders a name", () => {
-  render(<Person name="Jack" />);
+  render(<Person name="Ben" />);
   const divElement = screen.getByRole("contentinfo");
-  expect(divElement).toHaveTextContent("Name is Jack");
+  expect(divElement).toHaveTextContent("My Name is Ben");
   expect(divElement).toHaveAttribute("role", "contentinfo");
 });
--- src/Person.tsx
+++ src/Person.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Person = ({ name }: { name: string }) => (
-  <div role="contentinfo">Name is {name}</div>
+  <div role="contentinfo">My Name is {name}</div>
 );
