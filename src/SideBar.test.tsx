ad5be2b8545d57aa3001f0ea6f7a18886f991c0c
bnmrly <benjaminjohnmarley@gmail.com>
2024-04-08 18:23:09 +0100

    test array

--- src/SideBar.test.tsx
+++ src/SideBar.test.tsx
@@ -8,9 +8,13 @@ test("renders a name", () => {
       name: "test",
       href: "/test",
     },
+    {
+      name: "test",
+      href: "/benmarley.netlify.app",
+    },
   ];
   render(<SideBar items={items} />);
   const anchorElements = screen.getAllByRole("navigation");
-  expect(anchorElements[0]).toHaveTextContent(items[0].name);
-  expect(anchorElements[0]).toHaveAttribute("href", items[0].href);
+  expect(anchorElements[1]).toHaveTextContent(items[1].name);
+  expect(anchorElements[1]).toHaveAttribute("href", items[1].href);
 });
--- src/SideBar.tsx
+++ src/SideBar.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-export const SideBar = ({
-  items,
-}: {
-  items: {
-    name: string;
-    href: string;
-  }[];
-}) => (
+type Item = {
+  name: string;
+  href: string;
+};
+
+type SideBarProps = {
+  items: Item[];
+};
+
+export const SideBar = ({ items }: SideBarProps) => (
   <div>
     {items.map((item) => (
       <div key={item.href}>
