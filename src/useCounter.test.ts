efe69f45cdd5c76941977b9c218b01f37e72c0f5
bnmrly <benjaminjohnmarley@gmail.com>
2024-04-15 15:46:54 +0100

    testing custom hooks

--- src/useCounter.test.ts
+++ src/useCounter.test.ts
@@ -1,6 +1,8 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 import { useCounter } from "./useCounter";
 
+// See - https://github.com/testing-library/react-hooks-testing-library
+
 test("should increment", () => {
   const { result } = renderHook(() => useCounter());
 
--- src/useCounter.ts
+++ src/useCounter.ts
@@ -3,7 +3,10 @@ import { useState, useCallback } from "react";
 export function useCounter() {
   const [count, setCount] = useState(0);
 
-  const increment = useCallback(() => setCount((x) => x + 1), []);
+  const increment = useCallback(
+    () => setCount((prevCount) => prevCount + 1),
+    []
+  );
 
   return { count, increment };
 }
